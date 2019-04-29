74ed65d8bd11a3444ecc91c36147c07874901828
hpaves <henri.paves@gmail.com>
2019-04-29 08:47:58 +0300

    cron viga parandatud

--- add_cronjob_user_x_job_y.sh
+++ add_cronjob_user_x_job_y.sh
@@ -5,7 +5,7 @@
 # Manpage: bash add_cronjob_user_x_job_y.sh <user> "<*/5 * * * * /path/to/job -with args>"
 # Adding cron jobs in a script: https://stackoverflow.com/a/9625233
 
-if [[ ! $(crontab -l | grep "$2") ]]
+if [[ ! $(crontab -u $1 -l | grep "$2") ]]
 then
     echo -e "$(crontab -u $1 -l)\n$2" | crontab -u $1 -
     printf "The following cron job added to $1:\n$2\n"
